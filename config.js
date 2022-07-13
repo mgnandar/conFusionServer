fe7e48b259206f46955f174607895d0b22536f36
mgnandar <mgnandar290@gmail.com>
2022-07-13 13:23:58 +0630

    removed keys

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '822255505420090',
-        clientSecret: '20d67ca631c5f67fad0864d2214202ac'
+        clientId: ' ',
+        clientSecret: ' '
     }
 }
